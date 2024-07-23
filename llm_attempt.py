1f79f1902f6be4dd9d4cac83b04a19794f2873ef
Vinu <vinulakkur@gmail.com>
2024-07-22 19:29:47 -0700

    Housekeeping updates

--- llm_attempt.py
+++ llm_attempt.py
@@ -10,106 +10,44 @@ from langchain.schema import StrOutputParser
 from dotenv import load_dotenv
 
 
-# Mount Google Drive to access Colab storage
-# drive.mount('/content/drive')
+# Code below to get pages from PDFs / Comment out if not using
 
-# Directory containing PDFs
-
-wd = os.getcwd()
-pdf_directory = '\Preprocessing\Data\spelling_pb-grade-3.pdf'
-absolute_path = wd + pdf_directory
+# wd = os.getcwd()
+# pdf_directory = '\Preprocessing\Data\spelling_pb-grade-3.pdf'
+# absolute_path = wd + pdf_directory
 
 # Extracting the text either directly into a string or testing other ways
 # Function to extract text from a PDF file
-def extract_text_from_pdf(pdf_path):
-    pdf_document = fitz.open(pdf_path)
-    text_data = []
-    for page_num in range(len(pdf_document)):
-        page = pdf_document.load_page(page_num)
-        text = page.get_text()
-        text_data.append(text)
-    full_text = "\n".join(text_data)
-    return full_text
+# def extract_text_from_pdf(pdf_path):
+#     pdf_document = fitz.open(pdf_path)
+#     text_data = []
+#     for page_num in range(len(pdf_document)):
+#         page = pdf_document.load_page(page_num)
+#         text = page.get_text()
+#         text_data.append(text)
+#     full_text = "\n".join(text_data)
+#     return full_text
 
 
 # Function to preprocess the extracted text
-def preprocess_text(text):
-    text = re.sub(r'\s+', ' ', text)
-    chapters = text.split('CHAPTER')
-    return chapters
+# def preprocess_text(text):
+#     text = re.sub(r'\s+', ' ', text)
+#     chapters = text.split('CHAPTER')
+#     return chapters
 
 
 # full_text = extract_text_from_pdf(r'C:\Users\submi\PycharmProjects\Capstone_HomeworkHelpers\Preprocessing\Data\spelling_pb-grade-3.pdf')
 
+#Loading Environment Variables
 load_dotenv()
 
 openai_key = os.getenv('OPENAI_API_KEY')
-# os.environ['TAVILY_API_KEY'] = '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'
-# opaikey = 'sk-proj-dIlb8btdHMAupYJMSXHuT3BlbkFJm3KYbZOLkJ43TXr9Pjep'
-# os.environ['OPENAI_API_KEY'] = opaikey
 
-# embeddings = OpenAIEmbeddings()
-# print(type(embeddings))
+# Model setting
 
 model = ChatOpenAI()
-#
-# def create_retriever(text):
-#
-#     # docs = WebBaseLoader(urls).load()
-#     # loader = PyPDFLoader(pdf_path)
-#     # pages = loader.load_and_split()
-#     # print(type(pages))
-#     text_splitter = RecursiveCharacterTextSplitter()
-#     # documents = text_splitter.split_documents(docs)
-#     embeddings = OpenAIEmbeddings()
-#     db = FAISS.from_documents(pages, embeddings)
-#     print(type(db))
-#     # retriever = db.as_retriever()
-#     retriever = "hello"
-#     return retriever
-#
-# def create_retriever(pdf_path):
-#
-#     # docs = WebBaseLoader(urls).load()
-#     loader = PyPDFLoader(pdf_path)
-#     pages = loader.load_and_split()
-#     print(type(pages))
-#     # text_splitter = RecursiveCharacterTextSplitter()
-#     # documents = text_splitter.split_documents(docs)
-#     embeddings = OpenAIEmbeddings()
-#     db = FAISS.from_documents(pages, embeddings)
-#     print(type(db))
-#     # retriever = db.as_retriever()
-#     retriever = "hello"
-#     return retriever
-
-# retriever = create_retriever(pdf_directory)
-
-# print(type(retriever))
-#
-# template = (
-#     """
-#     Please give me an outline of the children's text book below:
-#
-#     {textbook}
-#     """
-# )
-#
-# prompt = ChatPromptTemplate.from_template((template))
-#
-# # query_cr_questions = f"What are some job interview questions for a {title} at {company}?"
-#
-# # chain_cr_questions = RunnableMap({
-# #     "context": lambda x: create_search_retriever(query_cr_questions).get_relevant_documents(
-# #         f"What are some job interview questions for a {title} at {company}?"),
-# #     "company": lambda x: x["company"],
-# #     "title": lambda x: x["title"]
-# # }) | prompt_cr_questions| model | output_parser
-#
 output_parser = StrOutputParser()
-# chain = prompt | model | output_parser
-#
-# print(chain.invoke({'textbook': full_text}))
+
 
 # Testing initial turning into homework problems:
 
@@ -159,23 +97,24 @@ blow
 
 style = "Sharks"
 
-template_2 = """
-Transform the set of problems in the children's textbook below into problems wrapped in a story about {style}. Make sure the problems are intertwined IN the narrative.
+template_conversion = """
+Transform the set of problems in the children's textbook below into problems wrapped in a story about {style}. 
+
+Make sure the problems are intertwined IN the narrative. Present the problems as problems that need to be solved. 
+
 Please preserve each homework problem as closely as possible, while maintaining the integrity of the story. Please feel free to keep the length of the output as long as necessary.
 Do not SOLVE the problems, but just preserve the actual problems for the student to solve. 
 
 {problems}
 """
 
-prompt_2 = ChatPromptTemplate.from_template(template_2)
+prompt_conversion = ChatPromptTemplate.from_template(template_conversion)
 
-chain_2 = prompt_2 | model | output_parser
-
-# narrative = chain_2.invoke({'style': style, 'problems': problem_test})
-# print(narrative)
+chain_conversion = prompt_conversion | model | output_parser
 
 
-challenger_template = """
+
+template_challenger = """
 Does the narrative below have an interesting plot? Please rate the story from 1 - 10, and then give me a paragraph-long feedback/explanation on why you gave it that rating, as well as resulting improvements you can make to make the narrative stronger.
 Use this format to do so: 
 Story Rating: _____
@@ -197,12 +136,10 @@ Problem Set:
 {problem_set}
 
 """
-prompt_challenger = ChatPromptTemplate.from_template(challenger_template)
+prompt_challenger = ChatPromptTemplate.from_template(template_challenger)
 chain_challenger = prompt_challenger | model | output_parser
 
-# challenger_feedback = chain_challenger.invoke({'narrative':narrative, 'problem_set': problem_test})
 
-# print(chain_challenger.invoke({'narrative':narrative, 'problem_set': problem_test}))
 
 
 template_feedback_incorporated = """
@@ -227,27 +164,13 @@ Lastly, before giving an answer, make SURE the problems themselves are included 
 #
 prompt_challenger_feedback = ChatPromptTemplate.from_template(template_feedback_incorporated)
 chain_challenger_feedback = prompt_challenger_feedback | model | output_parser
-# challenger_feedback_incorporated = chain_challenger_feedback.invoke({'feedback' : challenger_feedback, 'narrative': narrative, 'problem_set':problem_test})
-#
-# print(narrative)
-# print(challenger_feedback)
-# print(challenger_feedback_incorporated)
-
-#
-# narrative = chain_challenger_feedback.invoke({'feedback' : challenger_feedback, 'narrative': narrative, 'problem_set':problem_test})
-# challenger_feedback = chain_challenger.invoke({'narrative':narrative, 'problem_set': problem_test})
-# narrative = chain_challenger_feedback.invoke({'feedback' : challenger_feedback, 'narrative': narrative, 'problem_set':problem_test})
-# challenger_feedback = chain_challenger.invoke({'narrative':narrative, 'problem_set': problem_test})
-#
-# print(chain_challenger_feedback.invoke({'feedback' : challenger_feedback, 'narrative': narrative, 'problem_set':problem_test}))
-# print(chain_challenger.invoke({'narrative':narrative, 'problem_set': problem_test}))
-#
-
 
 def convert_homework(style, problems):
 
-    narrative = chain_2.invoke({'style': style, 'problems': problems})
-    # challenger_feedback = chain_challenger.invoke({'narrative':narrative, 'problem_set': problems})
-    # challenger_feedback_incorporated = chain_challenger_feedback.invoke({'feedback' : challenger_feedback, 'narrative': narrative, 'problem_set':problems})
+    narrative = chain_conversion.invoke({'style': style, 'problems': problems})
+    challenger_feedback = chain_challenger.invoke({'narrative':narrative, 'problem_set': problems})
+    challenger_feedback_incorporated = chain_challenger_feedback.invoke({'feedback' : challenger_feedback, 'narrative': narrative, 'problem_set':problems})
 
-    return narrative
+    return challenger_feedback_incorporated
+
+
